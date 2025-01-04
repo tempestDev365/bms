dd1c6e544c9fd43f4cbed56e14f92358274de3a5
Christian Alfonso <alfonso.christian.macatangay@gmail.com>
2025-01-05 03:04:00 +0800

    resident table added

--- views/admin/main.php
+++ views/admin/main.php
@@ -10,8 +10,8 @@ if(!isset($_SESSION['admin'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Brgy. Sinbanali</title>
-    <link rel="shortcut icon" href="../assets/img/logo-125.png" type="image/x-icon">
-    <link rel="stylesheet" href="../assets/css/main.css">
+    <link rel="shortcut icon" href="../../assets/img/logo-125.png" type="image/x-icon">
+    <link rel="stylesheet" href="../../assets/css/main.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
--- views/admin/residents.php
+++ views/admin/residents.php
@@ -4,9 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Brgy. Sinbanali</title>
-    <link rel="shortcut icon" href="../assets/img/logo-125.png" type="image/x-icon">
-    <link rel="stylesheet" href="../assets/css/main.css">
+    <link rel="shortcut icon" href="../../assets/img/logo-125.png" type="image/x-icon">
+    <link rel="stylesheet" href="../../assets/css/main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.bootstrap5.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/3.0.3/css/responsive.bootstrap5.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.3.6/css/buttons.bootstrap5.min.css">
+    <style>
+        table th, table td {
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
 
@@ -15,7 +24,7 @@
            
         </div>
 
-        <div class="admin-content flex-grow-1 p-4">
+        <div class="admin-content flex-grow-1 p-4 bg-light">
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb">
                   <li class="breadcrumb-item active" aria-current="page">BMS</li>
@@ -23,6 +32,68 @@
                 </ol>
               </nav>
               
+
+            <div class="container-fluid p-3 shadow-sm border rounded bg-white">
+                <h1 class="mb-3 text-center">Registered Residents</h1>
+                <table class="table table-bordered nowrap table-hover mt-3" id="example">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Gender</th>
+                            <th>Age</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>01</td>
+                            <td>John Doe</td>
+                            <td>Male</td>
+                            <td>21</td>
+                            <td>
+                                <button class="btn btn-primary">View</button>
+                                <button class="btn btn-danger">Delete</button>
+
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>01</td>
+                            <td>John Doe</td>
+                            <td>Male</td>
+                            <td>21</td>
+                            <td>
+                                <button class="btn btn-primary">View</button>
+                                <button class="btn btn-danger">Delete</button>
+
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>01</td>
+                            <td>John Doe</td>
+                            <td>Male</td>
+                            <td>21</td>
+                            <td>
+                                <button class="btn btn-primary">View</button>
+                                <button class="btn btn-danger">Delete</button>
+
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>01</td>
+                            <td>John Doe</td>
+                            <td>Male</td>
+                            <td>21</td>
+                            <td>
+                                <button class="btn btn-primary">View</button>
+                                <button class="btn btn-danger">Delete</button>
+
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
         </div>
         
     </div>
@@ -32,6 +103,58 @@
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.datatables.net/2.1.8/js/dataTables.js"></script>
+    <script src="https://cdn.datatables.net/2.1.8/js/dataTables.bootstrap5.js"></script>
+    <script src="https://cdn.datatables.net/responsive/3.0.3/js/dataTables.responsive.js"></script>
+    <script src="https://cdn.datatables.net/responsive/3.0.3/js/responsive.bootstrap5.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.3.6/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.bootstrap5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.print.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.colVis.min.js"></script>
     <script src="../components/sidebar.js" defer></script>
+    <script>  
+        new DataTable('#example', {
+            responsive: true,
+            dom: 'Bfrtip',
+            buttons: [
+                {
+                    extend: 'copy',
+                    exportOptions: {
+                        columns: ':not(:last-child)'
+                    }
+                },
+                {
+                    extend: 'csv',
+                    exportOptions: {
+                        columns: ':not(:last-child)'
+                    }
+                },
+                {
+                    extend: 'excel',
+                    exportOptions: {
+                        columns: ':not(:last-child)'
+                    }
+                },
+                {
+                    extend: 'pdf',
+                    exportOptions: {
+                        columns: ':not(:last-child)'
+                    },
+                    customize: function (doc) {
+                        doc.content[1].table.widths = Array(doc.content[1].table.body[0].length + 1).join('*').split('');
+                        doc.styles.tableHeader.alignment = 'center';
+                        doc.styles.tableBodyEven.alignment = 'center';
+                        doc.styles.tableBodyOdd.alignment = 'center';
+                    }
+                },
+                
+            ]
+        });
+    </script>
 </body>
 </html>
