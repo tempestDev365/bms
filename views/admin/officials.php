e1651e7efa052dc6c5d64f42438eb4c380c51954
Christian Alfonso <alfonso.christian.macatangay@gmail.com>
2025-01-05 20:55:23 +0800

    added officials

--- views/admin/officials.php
+++ views/admin/officials.php
@@ -1,34 +1,186 @@
-<?php
-session_start();
-if(!isset($_SESSION['admin'])) {
-    header('Location: adminLogin.php');
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Brgy. Sinbanali</title>
-    <link rel="shortcut icon" href="../assets/img/logo-125.png" type="image/x-icon">
-    <link rel="stylesheet" href="../assets/css/main.css">
+    <link rel="shortcut icon" href="../../assets/img/logo-125.png" type="image/x-icon">
+    <link rel="stylesheet" href="../../assets/css/main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.bootstrap5.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/3.0.3/css/responsive.bootstrap5.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.3.6/css/buttons.bootstrap5.min.css">
+    <style>
+        table th, table td {
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
 
-    <div class="main-container d-flex" style="min-height: 100vh; min-width: 100vw;">
+    <div class="main-container d-flex" style="min-height: 100vh; min-width: 100%">
         <div class="admin-sidebar">
            
         </div>
 
-        <div class="admin-content flex-grow-1 p-4">
+        <div class="admin-content flex-grow-1 p-4 bg-light">
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb">
                   <li class="breadcrumb-item active" aria-current="page">BMS</li>
-                  <li class="breadcrumb-item"><a href="./officials.html" class="text-dark">Officials</a></li>
+                  <li class="breadcrumb-item"><a href="./issuedClearance.html" class="text-dark">Residents</a></li>
                 </ol>
               </nav>
               
+
+            <div class="container-fluid p-3 shadow-sm border rounded bg-white">
+                <h1 class="mb-3 text-center">Barangay Officials</h1>
+
+                <table class="table table-bordered nowrap table-hover mt-3" id="example">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Position</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>01</td>
+                            <td>Caridad J. Sanchez</td>
+                            <td>Punong Barangay</td>
+                            <td>
+                                <button class="btn btn-primary">Edit</button>
+                                <button class="btn btn-danger">Delete</button>
+
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>02</td>
+                            <td>Nieves M, Dela Cruz</td>
+                            <td>Kagawad</td>
+                            <td>
+                                <button class="btn btn-primary">Edit</button>
+                                <button class="btn btn-danger">Delete</button>
+
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>03</td>
+                            <td>Ronald F. Marquez</td>
+                            <td>Kagawad</td>
+                            <td>
+                                <button class="btn btn-primary">Edit</button>
+                                <button class="btn btn-danger">Delete</button>
+
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>04</td>
+                            <td>Cesar R. Concepcion</td>
+                            <td>Kagawad</td>
+                            <td>
+                                <button class="btn btn-primary">Edit</button>
+                                <button class="btn btn-danger">Delete</button>
+
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>05</td>
+                            <td>Lolita E. Marquez</td>
+                            <td>Kagawad</td>
+                            <td>
+                                <button class="btn btn-primary">Edit</button>
+                                <button class="btn btn-danger">Delete</button>
+
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>06</td>
+                            <td>Angeline Rose D. Sanchez</td>
+                            <td>Kagawad</td>
+                            <td>
+                                <button class="btn btn-primary">Edit</button>
+                                <button class="btn btn-danger">Delete</button>
+
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>07</td>
+                            <td>Leo J. Ignacio</td>
+                            <td>Kagawad</td>
+                            <td>
+                                <button class="btn btn-primary">Edit</button>
+                                <button class="btn btn-danger">Delete</button>
+
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>08</td>
+                            <td>Ervin G. Ignacio</td>
+                            <td>Kagawad</td>
+                            <td>
+                                <button class="btn btn-primary">Edit</button>
+                                <button class="btn btn-danger">Delete</button>
+
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>09</td>
+                            <td>Juanise Raniel I. Ignacio</td>
+                            <td>SK Kagawad</td>
+                            <td>
+                                <button class="btn btn-primary">Edit</button>
+                                <button class="btn btn-danger">Delete</button>
+
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>10</td>
+                            <td>John Paul T. Grande</td>
+                            <td>Secretary</td>
+                            <td>
+                                <button class="btn btn-primary">Edit</button>
+                                <button class="btn btn-danger">Delete</button>
+
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>11</td>
+                            <td>Arthur B. Castor</td>
+                            <td>Treasurer</td>
+                            <td>
+                                <button class="btn btn-primary">Edit</button>
+                                <button class="btn btn-danger">Delete</button>
+
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>12</td>
+                            <td>Robert F. Cuevas</td>
+                            <td>Admin.</td>
+                            <td>
+                                <button class="btn btn-primary">Edit</button>
+                                <button class="btn btn-danger">Delete</button>
+
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>13</td>
+                            <td>Ramon G. Colmenar</td>
+                            <td>EX-0</td>
+                            <td>
+                                <button class="btn btn-primary">Edit</button>
+                                <button class="btn btn-danger">Delete</button>
+
+                            </td>
+                        </tr>
+                        
+                    </tbody>
+                </table>
+            </div>
+
         </div>
         
     </div>
@@ -38,7 +190,64 @@ if(!isset($_SESSION['admin'])) {
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.datatables.net/2.1.8/js/dataTables.js"></script>
+    <script src="https://cdn.datatables.net/2.1.8/js/dataTables.bootstrap5.js"></script>
+    <script src="https://cdn.datatables.net/responsive/3.0.3/js/dataTables.responsive.js"></script>
+    <script src="https://cdn.datatables.net/responsive/3.0.3/js/responsive.bootstrap5.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.3.6/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.bootstrap5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.print.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.colVis.min.js"></script>
     <script src="../components/sidebar.js" defer></script>
+    <script>  
+        new DataTable('#example', {
+            responsive: true,
+            dom: 'Brtip', 
+            buttons: [
+                {
+                    extend: 'copy',
+                    exportOptions: {
+                        columns: ':not(:last-child)'
+                    }
+                },
+                {
+                    extend: 'csv',
+                    exportOptions: {
+                        columns: ':not(:last-child)'
+                    }
+                },
+                {
+                    extend: 'excel',
+                    exportOptions: {
+                        columns: ':not(:last-child)'
+                    }
+                },
+                {
+                    extend: 'pdf',
+                    exportOptions: {
+                        columns: ':not(:last-child)'
+                    },
+                    customize: function (doc) {
+                        doc.content[1].table.widths = Array(doc.content[1].table.body[0].length + 1).join('*').split('');
+                        doc.styles.tableHeader.alignment = 'center';
+                        doc.styles.tableBodyEven.alignment = 'center';
+                        doc.styles.tableBodyOdd.alignment = 'center';
+                    }
+                },
+                // Removed search button
+            ]
+        });
 
+        $('#genderFilter').on('change', function() {
+            var filterValue = $(this).val();
+            var table = $('#example').DataTable();
+            table.column(2).search(filterValue === 'all' ? '' : filterValue, true, false).draw();
+        });
+    </script>
 </body>
 </html>
