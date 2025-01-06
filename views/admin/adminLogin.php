15611cd297204d6c9a30a7ceb8772db85e5f20af
Christian Alfonso <alfonso.christian.macatangay@gmail.com>
2025-01-06 18:19:25 +0800

    change path css of login admin

--- views/admin/adminLogin.php
+++ views/admin/adminLogin.php
@@ -11,7 +11,7 @@ if(isset($_SESSION['admin'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Login</title>
     <link rel="shortcut icon" href="../../assets/img/logo-125.png" type="image/x-icon">
-    <link rel="stylesheet" href="../assets/css/main.css">
+    <link rel="stylesheet" href="../../assets/css/main.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
@@ -20,7 +20,7 @@ if(isset($_SESSION['admin'])) {
         <div class="card p-4" style="width: 40rem;">
             <div class="card-title d-flex justify-content-center align-items-center" style="gap: 5px;">
                     <img src="../../assets/img/logo-125.png" class="img-fluid" style="width: 100px;" alt="">
-                <div class="card-logo">
+                <div class="card-logo">s
                     <h2>Login</h2>
                     <label>"Barangay Sinbanali, City of Bacoor"</label>
                 </div>
