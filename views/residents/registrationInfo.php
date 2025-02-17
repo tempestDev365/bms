e7f8287a84073c565c9e52cc4970a1dc8817e900
tempestDev365 <alfonso.christian.macatangay@gmail.com>
2025-02-17 23:10:01 +0800

    rvised registration

--- views/residents/registrationInfo.php
+++ views/residents/registrationInfo.php
@@ -20,11 +20,9 @@
         <ul class="pagination">
             <li class="page-item"><a class="page-link" href="#" id="page1">1</a></li>
             <li class="page-item"><a class="page-link" href="#" id="page2">2</a></li>
-            <li class="page-item"><a class="page-link" href="#" id="page3">3</a></li>
-            <li class="page-item"><a class="page-link" href="#" id="page4">4</a></li>
-            <li class="page-item"><a class="page-link" href="#" id="page5">5</a></li>
-            <li class="page-item"><a class="page-link" href="#" id="page6">6</a></li>
-            <li class="page-item"><a class="page-link" href="#" id="page7">7</a></li>
+            <li class="page-item"><a class="page-link" href="#" id="page4">3</a></li>
+            <li class="page-item"><a class="page-link" href="#" id="page5">4</a></li>
+            
         </ul>
 
         <!-- Form 1 -->
@@ -114,56 +112,7 @@
             </div>
         </div>
 
-        <!-- Form 3 -->
-        <div class="card p-3" id="form3" style="min-width: 500px; min-height: 500px; width: 500px;">
-            <div class="card-title">
-                <h2>IDENTITY VERIFICATION!</h2>
-            </div>
-            <div class="card-body">
-                <div class="upper-card">
-                    <label for="">Please upload barangay ID / valid ID</label>
-                    <div class="upper-card-img">
-                        <div class="img-group d-flex justify-content-center align-items-center">
-                            <div class="card-img">
-                                <img src="../../assets/img/good-id.png" alt="good" class="img-fluid">
-                            </div>
-                            <div class="card-img">
-                                <img src="../../assets/img/bad-img1.png" alt="bad-img1.png" class="img-fluid">
-                            </div>
-                            <div class="card-img">
-                                <img src="../../assets/img/bad-img2.png" alt="bad-img2.png" class="img-fluid">
-                            </div>
-                        </div>
-                        <div class="img-label d-flex justify-content-around align-items-center">
-                            <label for="">Good</label>
-                            <label for="">Bad</label>
-                            <label for="">Bad</label>
-                        </div>
-                    </div>
-                    <div class="middle-card mt-3 d-flex flex-column">
-                        <p>Make sure your ID is</p>
-                        <label>Valid</label>
-                        <label>Readable</label>
-                        <label>Original full-sized. Unedited Document</label>
-                        <label>No black and white images</label>
-                    </div>
-                    <div class="bottom-card mt-3">
-                        <form action="" class="d-flex justify-content-center align-items-center" style="gap: 5px;">
-                            <div class="form-group" style="flex-grow: 1;">
-                                <input type="file" name="frontID" hidden id="frontID">
-                                <label class="d-flex justify-content-center align-items-center" for="frontID" style="width: 100%; border: 1px dotted black; height: 200px; cursor: pointer;">Upload Front</label>
-                            </div>
-                            <div class="form-group" style="flex-grow: 1;">
-                                <input type="file" name="backID" hidden id="backID">
-                                <label class="d-flex justify-content-center align-items-center" for="backID" style="width: 100%; border: 1px dotted black; height: 200px; cursor: pointer;">Upload Back</label>
-                            </div>
-                        </form>
-                        <button class="btn btn-primary w-100 mt-3 p-3" style="border-radius: 20px;">NEXT</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-
+     
         <!-- Form 4 -->
         <div class="card p-3" id="form4" style="min-width: 500px; min-height: 500px; width: 500px;">
             <div class="card-title">
@@ -215,49 +164,7 @@
             </div>
         </div>
 
-        <!-- Form 6 -->
-        <div class="card p-3" id="form6" style="max-width: 600px; min-height: 600px; width: 600px;">
-            <div class="card-title">
-                <h2>Create your own MPIN</h2>
-                <label for="">Enter a 6 digit MPIN below</label>
-            </div>
-            <div class="card-pin mt-5 d-flex flex-column justify-content-center align-items-center" style="flex-grow: 1;">
-                <label for="">Set your MPIN</label>
-                <div class="pin-group d-flex justify-content-between align-items-center" style="gap: 5px;">
-                    <input type="number" style="max-width: 50px; max-height: 60px; height: 60px;" name="" id="">
-                    <input type="number" style="max-width: 50px; max-height: 60px; height: 60px;" name="" id="">
-                    <input type="number" style="max-width: 50px; max-height: 60px; height: 60px;" name="" id="">
-                    <input type="number" style="max-width: 50px; max-height: 60px; height: 60px;" name="" id="">
-                    <input type="number" style="max-width: 50px; max-height: 60px; height: 60px;" name="" id="">
-                    <input type="number" style="max-width: 50px; max-height: 60px; height: 60px;" name="" id="">
-                </div>
-            </div>
-            <div class="card-btn mt-3">
-                <button class="btn btn-primary w-100 p-3" style="border-radius: 20px;">CONFIRM</button>
-            </div>
-        </div>
-
-        <!-- Form 7 -->
-        <div class="card p-3" id="form7" style="max-width: 600px; min-height: 600px; width: 600px;">
-            <div class="card-title">
-                <h2>Re-Enter your own MPIN</h2>
-                <label for="">Enter a 6 digit MPIN below</label>
-            </div>
-            <div class="card-pin mt-5 d-flex flex-column justify-content-center align-items-center" style="flex-grow: 1;">
-                <label for="">Set your MPIN</label>
-                <div class="pin-group d-flex justify-content-between align-items-center" style="gap: 5px;">
-                    <input type="number" style="max-width: 50px; max-height: 60px; height: 60px;" name="" id="">
-                    <input type="number" style="max-width: 50px; max-height: 60px; height: 60px;" name="" id="">
-                    <input type="number" style="max-width: 50px; max-height: 60px; height: 60px;" name="" id="">
-                    <input type="number" style="max-width: 50px; max-height: 60px; height: 60px;" name="" id="">
-                    <input type="number" style="max-width: 50px; max-height: 60px; height: 60px;" name="" id="">
-                    <input type="number" style="max-width: 50px; max-height: 60px; height: 60px;" name="" id="">
-                </div>
-            </div>
-            <div class="card-btn mt-3">
-                <button class="btn btn-primary w-100 p-3" style="border-radius: 20px;">NEXT</button>
-            </div>
-        </div>
+       
 
     </main>
 
@@ -268,79 +175,37 @@
             event.preventDefault();
             document.getElementById('form1').style.display = 'block';
             document.getElementById('form2').style.display = 'none';
-            document.getElementById('form3').style.display = 'none';
             document.getElementById('form4').style.display = 'none';
             document.getElementById('form5').style.display = 'none';
-            document.getElementById('form6').style.display = 'none';
-            document.getElementById('form7').style.display = 'none';
         });
 
         document.getElementById('page2').addEventListener('click', function(event) {
             event.preventDefault();
             document.getElementById('form1').style.display = 'none';
             document.getElementById('form2').style.display = 'block';
-            document.getElementById('form3').style.display = 'none';
             document.getElementById('form4').style.display = 'none';
             document.getElementById('form5').style.display = 'none';
-            document.getElementById('form6').style.display = 'none';
-            document.getElementById('form7').style.display = 'none';
         });
 
-        document.getElementById('page3').addEventListener('click', function(event) {
-            event.preventDefault();
-            document.getElementById('form1').style.display = 'none';
-            document.getElementById('form2').style.display = 'none';
-            document.getElementById('form3').style.display = 'block';
-            document.getElementById('form4').style.display = 'none';
-            document.getElementById('form5').style.display = 'none';
-            document.getElementById('form6').style.display = 'none';
-            document.getElementById('form7').style.display = 'none';
-        });
+       
 
         document.getElementById('page4').addEventListener('click', function(event) {
             event.preventDefault();
             document.getElementById('form1').style.display = 'none';
             document.getElementById('form2').style.display = 'none';
-            document.getElementById('form3').style.display = 'none';
             document.getElementById('form4').style.display = 'block';
             document.getElementById('form5').style.display = 'none';
-            document.getElementById('form6').style.display = 'none';
-            document.getElementById('form7').style.display = 'none';
         });
 
         document.getElementById('page5').addEventListener('click', function(event) {
             event.preventDefault();
             document.getElementById('form1').style.display = 'none';
             document.getElementById('form2').style.display = 'none';
-            document.getElementById('form3').style.display = 'none';
             document.getElementById('form4').style.display = 'none';
             document.getElementById('form5').style.display = 'block';
-            document.getElementById('form6').style.display = 'none';
-            document.getElementById('form7').style.display = 'none';
         });
 
-        document.getElementById('page6').addEventListener('click', function(event) {
-            event.preventDefault();
-            document.getElementById('form1').style.display = 'none';
-            document.getElementById('form2').style.display = 'none';
-            document.getElementById('form3').style.display = 'none';
-            document.getElementById('form4').style.display = 'none';
-            document.getElementById('form5').style.display = 'none';
-            document.getElementById('form6').style.display = 'block';
-            document.getElementById('form7').style.display = 'none';
-        });
-
-        // Add event listener for page 7
-        document.getElementById('page7').addEventListener('click', function(event) {
-            event.preventDefault();
-            document.getElementById('form1').style.display = 'none';
-            document.getElementById('form2').style.display = 'none';
-            document.getElementById('form3').style.display = 'none';
-            document.getElementById('form4').style.display = 'none';
-            document.getElementById('form5').style.display = 'none';
-            document.getElementById('form6').style.display = 'none';
-            document.getElementById('form7').style.display = 'block';
-        });
+       
     </script>
 </body>
 </html>
