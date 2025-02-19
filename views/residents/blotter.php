2dddab83c45d288eeba53d70c770dced9d53b00a
Menard <perez.menard.nomiddlename@gmail.com>
2025-02-20 00:09:38 +0800

    Fixed blotter?

--- views/residents/blotter.php
+++ views/residents/blotter.php
@@ -1,8 +1,4 @@
-<?php
-if(!isset($_SESSION['user_id'])) {
-    header('Location: ./residentLogin.php');
-}
-?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
--- views/residents/residentConcerns.php
+++ views/residents/residentConcerns.php
@@ -134,7 +134,7 @@ $allConcerns = getAllConcerns($_SESSION['user_id']);
                         <a class="nav-link" href="./userDocument.php">Document Request</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="./blotter.php">Blotter</a>
+                        <a class="nav-link" href="./residentBlotter.php">Blotter</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="./userNotification.php">Notification</a>
