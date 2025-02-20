3b3c3d3928c06a762af95247200417ef0e9199d2
tempestDev365 <alfonso.christian.macatangay@gmail.com>
2025-02-20 19:05:08 +0800

    fixed

--- controllers/updateResidentController.php
+++ controllers/updateResidentController.php
@@ -1,4 +1,8 @@
 <?php
+
+ini_set("display_errors", "1");
+  error_reporting(E_ALL);
+
 session_start();
 function resizeImage($file, $max_width, $max_height) {
     list($width, $height) = getimagesize($file) ?? "";
