305c09b099972a3b9d3304b1662f6c629a82c086
Menard <perez.menard.nomiddlename@gmail.com>
2025-02-24 20:49:37 +0800

    Fixed

--- controllers/documentRequestController.php
+++ controllers/documentRequestController.php
@@ -17,7 +17,6 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
     $stmt->execute();
     header('Location: ../views/residents/userDocument.php');
 }
-<<<<<<< HEAD
 function checkIfdocumentRequestExist(){
     include '../database/databaseConnection.php';
     $resident_id = $_SESSION['user_id'];
@@ -32,21 +31,5 @@ function checkIfdocumentRequestExist(){
     }else{
         return false;
     }
-=======
-
-//remove if not
-// Add the search query for first name, last name, and middle name
-if($_SERVER['REQUEST_METHOD'] == "GET" && isset($_GET['search'])){
-    $search = $_GET['search'];
-    $qry = "SELECT * FROM residents WHERE (first_name LIKE ? OR last_name LIKE ? OR middle_name LIKE ? OR middle_name IS NULL)";
-    $stmt = $conn->prepare($qry);
-    $searchParam = "%$search%";
-    $stmt->bindParam(1, $searchParam);
-    $stmt->bindParam(2, $searchParam);
-    $stmt->bindParam(3, $searchParam);
-    $stmt->execute();
-    $results = $stmt->fetchAll();
-    // ...process results...
->>>>>>> cb71d8a71f68b7d622623382c126f673461884fb
 }
 ?>
